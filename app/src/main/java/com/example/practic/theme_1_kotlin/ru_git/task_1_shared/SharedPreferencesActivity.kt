e834e8645daf21ca3c695fb55b2544c037a903bf
Nik <kolya-buldog@inbox.ru>
2024-12-14 21:39:48 +0300

    c

--- app/src/main/java/com/example/practic/theme_1_kotlin/ru_git/task_1_shared/SharedPreferencesActivity.kt
+++ app/src/main/java/com/example/practic/theme_1_kotlin/ru_git/task_1_shared/SharedPreferencesActivity.kt
@@ -1,6 +1,7 @@
 package com.example.practic.theme_1_kotlin.ru_git.task_1_shared
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.practic.databinding.SharedActivityBinding
@@ -8,7 +9,11 @@ import com.example.practic.databinding.SharedActivityBinding
 class SharedPreferencesActivity : AppCompatActivity() {
 
     private lateinit var binding: SharedActivityBinding
-    val prefs = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+    /* var pref: SharedPreferences
+
+    init {
+        pref = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+    }*/
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,33 +31,27 @@ class SharedPreferencesActivity : AppCompatActivity() {
     }
 
     private fun saveValue() {
-        val valueToSave = binding.saveValue.text.toString()
+        val pref = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
 
-        prefs.saveAsync(
+        val valueToSave = binding.saveValue.text.toString()
+        pref.saveAsync(
             key = "kolya",
             value = valueToSave,
             useCommit = false,
 
-            onSuccess = { binding.getValue.text = TODO() },
-
-            onError = { e -> binding.getValue.text = TODO() }
+            onSuccess = { binding.saveValue.setText("") },
+            onError = { e, s -> binding.saveValue.setText("") }
         )
     }
 
     private fun getValue() {
-        prefs.loadAsync(
+        val pref = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+
+        pref.loadAsync(
             key = "kolya",
             defaultValue = "DefaultUser ",
-
-            onResult = { result ->
-
-                binding.getValue.text = TODO()
-            },
-
-            onError = { e ->
-
-                binding.getValue.text = TODO()
-            }
+            onResult = { binding.getValue.setText(it.toString()) },
+            onError = { e -> binding.getValue.setText(e.toString()) }
         )
     }
 }
