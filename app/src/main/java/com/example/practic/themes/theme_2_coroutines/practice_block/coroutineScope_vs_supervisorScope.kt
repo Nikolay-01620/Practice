06681a6b568aea8d784fff53e359a3a014109571
Nik <kolya-buldog@inbox.ru>
2025-01-15 22:15:42 +0300

    Completed the practical block

--- app/src/main/java/com/example/practic/themes/theme_2_coroutines/practice_block/coroutineScope_vs_supervisorScope.kt
+++ app/src/main/java/com/example/practic/themes/theme_2_coroutines/practice_block/coroutineScope_vs_supervisorScope.kt
@@ -1,71 +1,95 @@
-/*
-package com.example.practic.theme_2_coroutines.practice_block
+package com.example.practic.themes.theme_2_coroutines.practice_block
 
 import kotlinx.coroutines.*
 
 fun main() = runBlocking {
 
 
-    println(runAsyncTasksWithSupervisor())
-    //println(runAsyncTasksWithCoroutineScope())
+    // println(runAsyncTasksWithSupervisor())
+    println(runAsyncTasksWithCoroutineScope())
 
 
 }
-
 
 suspend fun runAsyncTasksWithSupervisor(): String {
     val supervisorJob = SupervisorJob()  // Используем SupervisorJob для изоляции ошибок
     val scope = CoroutineScope(Dispatchers.Default + supervisorJob)
 
-    return try {
-        scope.launch {
-            val job1 = scope.async {
-                println("Task 1 started")
-                delay(1000)
-                println("Task 1 completed")
-            }
-
-            val job2 = scope.async {
-                println("Task 2 started")
-                delay(500)
-                throw Exception("Error in Task 2")
-            }
-
-            job1.await() // Не отменяется, несмотря на ошибку в job2
-            job2.await() // Проброс исключения, но другие задачи не будут отменены
-
-
-        }.join()
-        "runAsyncTasksWithSupervisor completed"
-
-    } catch (e: Exception) {
-        "Exception caught: ${e.message}"
-    }
-
-}
-
-
-suspend fun runAsyncTasksWithCoroutineScope(): String {
-
-    return try {
-        coroutineScope {
-            val job1 = async {
-                println("Task 1 started")
-                delay(1000)
-                println("Task 1 completed")
-            }
-            val job2 = async {
-                println("Task 2 started")
-                delay(1000)
-                throw Exception("Error in Task 2")
-            }
-
-            job1.await()
-            job2.await()
+    scope.launch {
+        val job1 = scope.async {
+            println("Task 1 started")
+            delay(1000)
+            println("Task 1 completed")
         }
 
-    } catch (e: Exception) {
-        "Exception caught: ${e.message}"
-    }
+        val job2 = scope.async {
+            println("Task 2 started")
+            throw Exception("Error in Task 2")
+        }
+
+        job1.await()
+        job2.await()
+    }.join() // Если тут не вызвать ожидание, корутины не выполнятся
+
+
+    return "runAsyncTasksWithSupervisor completed"
 }
-*/
+
+
+/** Для обработки ошибок без supervisorJob нужно обрабатывать ошибки отдельно в каждой корутине
+иначе отмена родительского скоупа,в случае проброса ошибки в одной из дочерних  */
+suspend fun runAsyncTasksWithCoroutineScope(): String {
+
+    coroutineScope {
+        val job1 = async {
+            try {
+                println("Task 1 started")
+                delay(1000)
+                println("Task 1 completed")
+            } catch (e: Exception) {
+                println("Error in Task 1: ${e.message}")
+            }
+        }
+
+        val job2 = async {
+            try {
+                println("Task 2 started")
+                delay(1000)
+                throw Exception("Error in Task 2")
+            } catch (e: Exception) {
+                println("Error in Task 2: ${e.message}")
+            }
+        }
+
+        // Ждем завершения всех задач
+        job1.await()
+        job2.await()
+    }
+    return "runAsyncTasksWithCoroutineScope completed"
+}
+
+
+
+/** После проброса ошибки возврата из род.скоупа не будет  */
+suspend fun runAsyncTasksWithCoroutineScope1(): String {
+
+    coroutineScope {
+
+        val job1 = async {
+            println("Task 1 started")
+            delay(1000)
+        }
+
+        val job2 = async {
+            println("Task 2 started")
+            delay(1000)
+            throw Exception("Error in Task 2")
+        }
+
+        // Ждем завершения всех задач
+        job1.await()
+        job2.await()
+    }
+    return "runAsyncTasksWithCoroutineScope completed"
+}
+
