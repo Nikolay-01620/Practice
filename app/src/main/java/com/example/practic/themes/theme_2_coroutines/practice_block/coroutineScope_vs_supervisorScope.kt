6cd33053aaf167e7ca53931338d5c45dab5d8b2b
Nik <kolya-buldog@inbox.ru>
2025-01-15 22:17:24 +0300

    Completed the practical block

--- app/src/main/java/com/example/practic/themes/theme_2_coroutines/practice_block/coroutineScope_vs_supervisorScope.kt
+++ app/src/main/java/com/example/practic/themes/theme_2_coroutines/practice_block/coroutineScope_vs_supervisorScope.kt
@@ -70,6 +70,7 @@ suspend fun runAsyncTasksWithCoroutineScope(): String {
 
 
 
+
 /** После проброса ошибки возврата из род.скоупа не будет  */
 suspend fun runAsyncTasksWithCoroutineScope1(): String {
 
