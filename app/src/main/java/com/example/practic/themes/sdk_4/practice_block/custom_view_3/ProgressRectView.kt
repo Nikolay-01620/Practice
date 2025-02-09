eb8368b3d5ee13f39bec5147298f189cea6d9adf
Nik <kolya-buldog@inbox.ru>
2025-02-09 14:36:53 +0300

    c

---
+++ app/src/main/java/com/example/practic/themes/sdk_4/practice_block/custom_view_3/ProgressRectView.kt
@@ -0,0 +1,61 @@
+package com.example.practic.themes.sdk_4.practice_block.`custom_view_3\`
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import kotlin.random.Random
+
+class ProgressRectView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+
+    private var progress = 0f  // Прогресс от 0% до 100%
+    private val maxProgress = 100f
+    private val paint = Paint()
+    private val progressRect = RectF()
+    
+    // Инициализация случайного цвета
+    private val randomColor get() = Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256))
+
+    init {
+        paint.isAntiAlias = true
+        paint.color = randomColor  // Изначальный случайный цвет
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        
+        // Отрисовываем сам прямоугольник
+        canvas.drawColor(Color.BLUE)  // Цвет фона
+        val width = width.toFloat()
+        val height = height.toFloat()
+
+        // Задаем область, где будет рисоваться прогресс
+        progressRect.set(0f, 0f, width * (progress / maxProgress), height)
+
+        // Отрисовываем заполненную область
+        canvas.drawRect(progressRect, paint)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                // Увеличиваем прогресс на 10%
+                progress += 10f
+                if (progress > maxProgress) {
+                    progress = 0f  // Сбрасываем прогресс, если он больше 100%
+                }
+                // Меняем цвет на случайный
+                paint.color = randomColor
+
+                invalidate()  // Перерисовываем вью
+            }
+        }
+        return true
+    }
+}
--- app/src/main/java/com/example/practic/themes/sdk_4/practice_block/work_manager_2/MainActivity.kt
+++ app/src/main/java/com/example/practic/themes/sdk_4/practice_block/work_manager_2/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.example.practic.themes.sdk_4.practice_block.work_manager_2
 
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.work.Constraints
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
-import com.example.practic.R
+import com.example.practic.databinding.SdkActivityWorkManagerBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: SdkActivityWorkManagerBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.sdk_activity_work_manager)
-        val btnClick=findViewById<Button>(R.id.btnClick)
+        binding = SdkActivityWorkManagerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val constraints = Constraints.Builder()
             .setRequiresCharging(true)
@@ -24,7 +26,7 @@ class MainActivity : AppCompatActivity() {
             .setConstraints(constraints)
             .build()
 
-        btnClick.setOnClickListener {
+        binding.btnClick.setOnClickListener {
 
             WorkManager.getInstance(this).enqueue(request)
         }
@@ -33,11 +35,8 @@ class MainActivity : AppCompatActivity() {
             .observe(this, Observer {
 
                 val status: String = it.state.name
-                Toast.makeText(this,status, Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, status, Toast.LENGTH_SHORT).show()
             })
 
     }
-
-
-
 }
