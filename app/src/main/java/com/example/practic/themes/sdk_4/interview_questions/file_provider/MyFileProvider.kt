8c46595061e1f4848235157746958dc13b086cf3
Nik <kolya-buldog@inbox.ru>
2025-02-24 18:07:48 +0300

    c

--- app/src/main/java/com/example/practic/themes/sdk_4/interview_questions/file_provider/MyFileProvider.kt
+++ app/src/main/java/com/example/practic/themes/sdk_4/interview_questions/file_provider/MyFileProvider.kt
@@ -16,7 +16,7 @@ class MyFileProvider : FileProvider() {
     // Метод для генерации URI
     fun shareFile(context: Context, fileName: String, authority: String): Uri? {
         // Получаем путь к файлу
-        val filePath = File(context.filesDir, "my_folder")
+        val filePath = File(context.filesDir, "my_images")
         val file = File(filePath, fileName)
 
         // Инициализируем и запускаем FileObserver для отслеживания изменений
