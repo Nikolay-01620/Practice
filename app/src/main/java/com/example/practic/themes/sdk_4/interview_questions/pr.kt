d69dafc1c8b996dfdd65df5af1b7a4d13575087f
Nik <kolya-buldog@inbox.ru>
2025-02-25 18:23:29 +0300

    c

--- app/src/main/java/com/example/practic/themes/sdk_4/interview_questions/pr.kt
+++ app/src/main/java/com/example/practic/themes/sdk_4/interview_questions/pr.kt
@@ -1,11 +1,12 @@
 package com.example.practic.themes.sdk_4.interview_questions
 
+import android.app.ActivityManager
 import android.content.Context
+import android.os.Parcelable
+import androidx.core.content.ContextCompat.getSystemService
+import java.io.Serializable
 
 fun main() {
-}
-
-
-class Context2(context: Context) {
 
 }
+
